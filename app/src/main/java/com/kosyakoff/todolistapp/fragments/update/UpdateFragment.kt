1b02e86eefdc7cad937c2b7a95c4cf3f9eb46ac6
kosyakoff1 <asondor@salusoptima.com>
2021-12-06 03:43:08 +0700

    Added navigation skeleton

---
+++ app/src/main/java/com/kosyakoff/todolistapp/fragments/update/UpdateFragment.kt
@@ -0,0 +1,20 @@
+package com.kosyakoff.todolistapp.fragments.update
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kosyakoff.todolistapp.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+}
