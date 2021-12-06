39a10dd2c8fbae96e995bcb26a84281e175bf07c
kosyakoff1 <asondor@salusoptima.com>
2021-12-07 04:26:27 +0700

    Added menus to add and update fragments

--- app/src/main/java/com/kosyakoff/todolistapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/kosyakoff/todolistapp/fragments/update/UpdateFragment.kt
@@ -1,9 +1,7 @@
 package com.kosyakoff.todolistapp.fragments.update
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.kosyakoff.todolistapp.R
 
@@ -13,8 +11,14 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
 }
